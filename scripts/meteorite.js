f1e79ff343b2bff57dfbf475f25856ab725603a1
Schzna <16777216bit@gmail.com>
2021-12-14 02:08:06 +0900

    introduce materials for jekyll rename "script" folder to "scripts"

--- scripts/meteorite.js
+++ scripts/meteorite.js
@@ -1,12 +1,10 @@
-const parser = new DOMParser();
-
-function start_starline() {
-    const next = 2000 + Math.random() * 500;
-    const y = -400 + Math.random() * 1000;
+function start_starline(svg) {
+    const next = 1000 + Math.random() * 500;
+    const y = -900 + Math.random() * 1600;
     const l = 100 + Math.random() * 300;
     const w = 3 + Math.random() * 10;
     const duration = 1000 + Math.random() * 500;
-    const svg = document.body.querySelector(".starline svg");
+    
     const elm = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
     elm.setAttribute("rx", (w/2).toString());
     elm.setAttribute("ry", (w/2).toString());
@@ -16,22 +14,25 @@ function start_starline() {
     elm.setAttribute("height", w.toString());
     elm.setAttribute("style", `animation-duration: ${duration}ms;`);
     svg.appendChild(elm);
-    setTimeout(end_starline, next, elm);
+    setTimeout(end_starline, next, elm, svg); 
+    
 }
 
-function end_starline(elm) {
+function end_starline(elm, svg) {
     elm.remove();
-    start_starline();
+    start_starline(svg);
 }
+
+const scale = 1.5;
 
 function start_meteorite() {
     const next = 100 + Math.random() * 100;
-    const x = 400 + Math.random() * 10 + scrollX;
-    const y = 200 + Math.random() * 10 + screenY;
+    const x = window.innerWidth/2.0 + Math.random() * 10 + scrollX;
+    const y = window.innerHeight/3.0 + Math.random() * 10 + screenY;
     const a = 30 + Math.random() * 10;
     const meteo = document.querySelector(".meteorite svg g");
     meteo.setAttribute("style", `
-        transform: translateX(${x}px) translateY(${y}px) rotate(${a}deg);
+        transform: translateX(${x}px) translateY(${y}px) rotate(${a}deg) scaleX(${scale}) scaleY(${scale});
     `);
     
     setTimeout(start_meteorite, next);
@@ -67,10 +68,11 @@ function end_meteorite_impact(e, type) {
 
 document.addEventListener("DOMContentLoaded", e => {
     start_meteorite();
-    for (let index = 0; index < 30; index++) {
-        const delay = Math.random() * 3000;
-        const type = Math.floor(Math.random()*2) + 1;
-        setTimeout(start_starline, delay);
-    }
+    document.querySelectorAll(".starline svg").forEach(svg => {
+        for (let index = 0; index < 30; index++) {
+            const delay = Math.random() * 3000;
+            setTimeout(start_starline, delay, svg);
+        }
+    });
 
 });
