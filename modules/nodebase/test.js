a7acbddf0a8500d5f2c76c730767280ba1838871
schzna <16777216bit@gmail.com>
2025-11-19 00:06:22 +0900

    minor update

---
+++ modules/nodebase/test.js
@@ -0,0 +1,13 @@
+function draw_nodebase(nodebase) {
+	if (nodebase != null) {
+		
+	}
+}
+
+function main() {
+	draw_nodebase(document.querySelector("nodebase"));
+}
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+	main();
+});
