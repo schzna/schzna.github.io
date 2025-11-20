119202ce97457057f10327ade140d2a53c65713d
schzna <16777216bit@gmail.com>
2025-11-21 04:27:05 +0900

    implement nodebase write 2025-11-21-diary add google material symbol

--- modules/nodebase/test.js
+++ modules/nodebase/test.js
@@ -1,3 +1,20 @@
+function pathd2obj(d) {
+    return d
+        .split(" ")
+        .map((e) => e.replace(",", ""))
+        .filter((e) => e != "")
+        .reduce((s, e) => {
+            const n = Number(e);
+            if (isNaN(n)) {
+                return s.concat([[e]]);
+            } else {
+                let ss = s;
+                ss.at(-1).push(n);
+                return ss;
+            }
+        }, []);
+}
+
 class Node {
     constructor(
         svg,
@@ -17,6 +34,8 @@ class Node {
         this.y = y;
         this.w = w;
         this.h = h;
+
+        this.adj = [];
 
         this.element = document.createElementNS(
             "http://www.w3.org/2000/svg",
@@ -42,12 +61,20 @@ class Node {
                 "http://www.w3.org/2000/svg",
                 "circle"
             );
-
-            input.setAttribute("cx", 0);
-            input.setAttribute("cy", h / 2);
+            const cx = 0;
+            const cy = (h * (i + 1)) / (inputs.length + 1);
+            input.setAttribute("cx", cx);
+            input.setAttribute("cy", cy);
             input.setAttribute("r", 5);
 
             this.element.appendChild(input);
+
+            this.ports[id] = {
+                owner: this.id,
+                name: name,
+                relx: cx,
+                rely: cy,
+            };
         });
 
         outputs.forEach((e, i) => {
@@ -56,12 +83,19 @@ class Node {
                 "http://www.w3.org/2000/svg",
                 "circle"
             );
-
-            output.setAttribute("cx", w);
-            output.setAttribute("cy", h / 2);
+            const cx = w;
+            const cy = (h * (i + 1)) / (outputs.length + 1);
+            output.setAttribute("cx", cx);
+            output.setAttribute("cy", cy);
             output.setAttribute("r", 5);
 
             this.element.appendChild(output);
+            this.ports[id] = {
+                owner: this.id,
+                name: name,
+                relx: cx,
+                rely: cy,
+            };
         });
 
         const label = document.createElementNS(
@@ -74,57 +108,256 @@ class Node {
 
         this.element.appendChild(label);
 
+        this.element.selected = false;
+
+        this.element.onmousedown = (e) => {
+            this.selected = true;
+            this.element.setAttribute("sel", "true");
+            this.element.parentElement.appendChild(this.element);
+            this.selpos = [e.clientX, e.clientY];
+        };
+
+        this.element.ontouchstart = (e) => {
+            this.selected = true;
+            this.element.setAttribute("sel", "true");
+            this.element.parentElement.appendChild(this.element);
+            this.selpos = [e.touches[0].clientX, e.touches[0].clientY];
+        };
+
+        this.element.onmousemove = (e) => {
+            if (this.selected) {
+                const [difx, dify] = [
+                    e.clientX - this.selpos[0],
+                    e.clientY - this.selpos[1],
+                ];
+
+                this.x = difx + this.x;
+                this.y = dify + this.y;
+                const T = this.owner.createSVGTransform();
+                T.setTranslate(Math.round(this.x), Math.round(this.y));
+                this.element.transform.baseVal.initialize(T);
+                for (const po of this.adj) {
+                    const info = pathd2obj(po.elem.getAttribute("d"));
+
+                    let x2 = info[1][3];
+                    let x_to = info[1][5];
+                    let y2 = info[1][4];
+                    let y_to = info[1][6];
+
+                    let x1 = info[1][1];
+                    let x_from = info[0][1];
+                    let y1 = info[1][2];
+                    let y_from = info[0][2];
+
+                    if (po.dir == "in") {
+                        x2 += difx;
+                        x_to += difx;
+                        y2 += dify;
+                        y_to += dify;
+                    } else {
+                        x1 += difx;
+                        x_from += difx;
+                        y1 += dify;
+                        y_from += dify;
+                    }
+
+                    let cmd = "";
+                    cmd += ` M ${x_from} ${y_from}`;
+                    cmd += ` C ${x1} ${y1}, ${x2} ${y2}, ${x_to} ${y_to}`;
+                    po.elem.setAttribute("d", cmd);
+                }
+                this.selpos = [e.clientX, e.clientY];
+            }
+        };
+
+        this.element.ontouchmove = (e) => {
+            if (this.selected) {
+                const [difx, dify] = [
+                    e.touches[0].clientX - this.selpos[0],
+                    e.touches[0].clientY - this.selpos[1],
+                ];
+
+                this.x = Math.round(difx + this.x);
+                this.y = Math.round(dify + this.y);
+                const T = this.owner.createSVGTransform();
+                T.setTranslate(this.x, this.y);
+                this.element.transform.baseVal.initialize(T);
+                for (const po of this.adj) {
+                    const info = pathd2obj(po.elem.getAttribute("d"));
+
+                    let x2 = info[1][3];
+                    let x_to = info[1][5];
+                    let y2 = info[1][4];
+                    let y_to = info[1][6];
+
+                    let x1 = info[1][1];
+                    let x_from = info[0][1];
+                    let y1 = info[1][2];
+                    let y_from = info[0][2];
+
+                    if (po.dir == "in") {
+                        x2 += difx;
+                        x_to += difx;
+                        y2 += dify;
+                        y_to += dify;
+                    } else {
+                        x1 += difx;
+                        x_from += difx;
+                        y1 += dify;
+                        y_from += dify;
+                    }
+
+                    let cmd = "";
+                    cmd += ` M ${x_from} ${y_from}`;
+                    cmd += ` C ${x1} ${y1}, ${x2} ${y2}, ${x_to} ${y_to}`;
+                    po.elem.setAttribute("d", cmd);
+                }
+                this.selpos = [e.touches[0].clientX, e.touches[0].clientY];
+            }
+        };
+
+        this.element.onmouseup = (e) => {
+            this.selected = false;
+            this.element.removeAttribute("sel");
+        };
+
+        this.element.ontouchend = (e) => {
+            this.selected = false;
+            this.element.removeAttribute("sel");
+        };
+
+        this.element.ontouchend = this.element.onmouseup;
+
         const T = this.owner.createSVGTransform();
         T.setTranslate(x, y);
-        this.element.transform.baseVal.appendItem(T);
+        this.element.transform.baseVal.initialize(T);
     }
 }
 
-function draw_nodebase(nodebase) {
-    if (nodebase != null) {
-        const parent = nodebase.parentElement;
-        const root = document.createElementNS(
+class NodebaseSystem {
+    constructor(base) {
+        this.parent = base.parentElement;
+        this.dom = document.createElementNS(
             "http://www.w3.org/2000/svg",
             "svg"
         );
-        root.setAttribute("id", "nodebase-root");
-        root.setAttribute("width", "700");
-        root.setAttribute("height", "300");
-        root.setAttribute("version", "1.1");
+        this.parent.insertBefore(this.dom, base);
 
-        Array.from(nodebase.children).forEach((elem, i) => {
+        const vh = document.documentElement.clientWidth * 0.7;
+        const vw = document.documentElement.clientWidth * 0.8;
+
+        this.connections = [];
+        this.ports = {};
+        this.nodes = {};
+        const node_elems = Array.from(base.children);
+        node_elems.forEach((elem, i) => {
             const type = elem.tagName;
             const name = elem.getAttribute("name") || type;
             const id = elem.getAttribute("id") || `node-${i}`;
 
+            const w = vw / 5;
+            const h = vh / 4;
+
             const inputs = Array.from(elem.querySelectorAll("in")).map(
                 (e, i) => {
-                    const e_id = `${id}[${i}]`;
+                    const e_id = `${id}.in[${i}]`;
+                    const from = e.getAttribute("from");
+                    if (from != undefined) {
+                        this.connections.push([from, e_id]);
+                    }
                     return [e_id, e.innerHTML || e_id];
                 }
             );
 
             const outputs = Array.from(elem.querySelectorAll("out")).map(
                 (e, i) => {
-                    const e_id = `${id}[${i}]`;
+                    const e_id = `${id}.out[${i}]`;
                     return [e_id, e.innerHTML || e_id];
                 }
             );
 
             const node = new Node(
-                root,
+                this.dom,
                 id,
                 name,
                 inputs,
                 outputs,
-                100 + i * 250,
-                50
+                ((vw - node_elems.length * w) * (i + 1)) /
+                    (node_elems.length + 1) +
+                    w * i,
+                ((vh - node_elems.length * h) * (i + 1)) /
+                    (node_elems.length + 1) +
+                    h * i,
+                w,
+                h
             );
-            root.appendChild(node.element);
+
+            this.nodes[id] = node;
+            this.ports = { ...this.ports, ...node.ports };
+            this.dom.appendChild(node.element);
+        });
+        base.remove();
+
+        this.connections.forEach((e) => {
+            const [from_id, to_id] = e;
+            const from = this.ports[from_id];
+            const from_node = this.nodes[from["owner"]];
+            const to = this.ports[to_id];
+            const to_node = this.nodes[to["owner"]];
+
+            const path = document.createElementNS(
+                "http://www.w3.org/2000/svg",
+                "path"
+            );
+
+            const from_x = from_node["x"] + from["relx"];
+            const from_y = from_node["y"] + from["rely"];
+
+            const to_x = to_node["x"] + to["relx"];
+            const to_y = to_node["y"] + to["rely"];
+
+            const rr = 10;
+
+            const x1 = (from_x + to_x * rr) / (rr + 1);
+            const y1 = from_y;
+
+            const x2 = (from_x * rr + to_x) / (rr + 1);
+            const y2 = to_y;
+
+            let cmd = "";
+            cmd += ` M ${from_x} ${from_y}`;
+            cmd += ` C ${x1} ${y1}, ${x2} ${y2}, ${to_x} ${to_y}`;
+
+            path.setAttribute("d", cmd);
+
+            let pathobj_from = {
+                elem: path,
+                dir: "out",
+            };
+            from_node.adj.push(pathobj_from);
+
+            let pathobj_to = {
+                elem: path,
+                dir: "in",
+            };
+            to_node.adj.push(pathobj_to);
+
+            this.dom.appendChild(path);
         });
 
-        parent.insertBefore(root, nodebase);
-        nodebase.remove();
+        this.dom.setAttribute("id", "nodebase-root");
+        this.dom.setAttribute("height", `${vh}px`);
+        this.dom.setAttribute("width", `${vw}px`);
+        this.dom.setAttribute("version", "1.1");
+    }
+
+    update_node(id, x, y) {}
+}
+
+function draw_nodebase(nodebase) {
+    if (nodebase != null) {
+        const ns = new NodebaseSystem(nodebase);
+        sessionStorage.setItem("ns", JSON.stringify(ns));
     }
 }
 
--- scripts/category.js
+++ scripts/category.js
@@ -1,7 +1,7 @@
 function pageselect(page) {
     const num_entries = 8;
     Array.from(document.querySelector("notes").children).forEach((elem, i) => {
-        if (i < (page-1) * num_entries || i >= ((page) * num_entries)) {
+        if (i < (page - 1) * num_entries || i >= page * num_entries) {
             elem.remove();
         }
     });
@@ -9,73 +9,89 @@ function pageselect(page) {
 
 function display_pagenavigator(category, page, num_pages) {
     const nav = document.querySelectorAll("page-navigator")[0];
-    nav.querySelectorAll("#page-first").forEach(elem => { 
+    nav.querySelectorAll("#page-first").forEach((elem) => {
         if (page == 1) {
             elem.innerHTML = "  ";
             elem.setAttribute("href", "#");
         } else {
-            `./notes.html?category=${category}&page=1`;
+            elem.setAttribute(
+                "href",
+                `./notes.html?category=${category}&page=1`
+            );
         }
     });
 
-    nav.querySelectorAll("#page-last").forEach(elem => { 
+    nav.querySelectorAll("#page-last").forEach((elem) => {
         if (page == num_pages) {
             elem.innerHTML = "  ";
             elem.setAttribute("href", "#");
         } else {
-            elem.setAttribute("href", `./notes.html?category=${category}&page=${num_pages}`);
+            elem.setAttribute(
+                "href",
+                `./notes.html?category=${category}&page=${num_pages}`
+            );
         }
     });
 
-    nav.querySelectorAll("#page-prev").forEach(elem => {
+    nav.querySelectorAll("#page-prev").forEach((elem) => {
         if (page == 1) {
             elem.innerHTML = "  ";
             elem.setAttribute("href", "#");
         } else {
-            elem.setAttribute("href", `./notes.html?category=${category}&page=${page-1}`);
+            elem.setAttribute(
+                "href",
+                `./notes.html?category=${category}&page=${page - 1}`
+            );
         }
     });
-    nav.querySelectorAll("#page-indicator").forEach(elem => { 
+    nav.querySelectorAll("#page-indicator").forEach((elem) => {
         elem.innerHTML = `${page}/${num_pages}`;
     });
-    nav.querySelectorAll("#page-next").forEach(elem => { 
+    nav.querySelectorAll("#page-next").forEach((elem) => {
         if (page == num_pages) {
             elem.innerHTML = "  ";
             elem.setAttribute("href", "#");
         } else {
-            elem.setAttribute("href", `./notes.html?category=${category}&page=${page+1}`);
+            elem.setAttribute(
+                "href",
+                `./notes.html?category=${category}&page=${page + 1}`
+            );
         }
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const params = new URLSearchParams(document.location.search.substring(1));
-    category = params.get("category")
+    category = params.get("category");
     if (category === null) {
-        category = "all"
+        category = "all";
     }
 
-    Array.from(document.body.children).forEach(elem => {
+    Array.from(document.body.children).forEach((elem) => {
         if (elem.tagName === "NOTES" && elem.id != category) {
-            elem.remove()
+            elem.remove();
         }
     });
 
-    const num_notes = Array.from(document.querySelector("notes").children).length;
+    const num_notes = Array.from(
+        document.querySelector("notes").children
+    ).length;
 
-    Array.from(document.querySelector("categories").children).forEach(elem => { 
-        if (elem.id === category) {
-            elem.setAttribute("id", "selected")
-        } else {
-            elem.setAttribute("id", "")
+    Array.from(document.querySelector("categories").children).forEach(
+        (elem) => {
+            if (elem.id === category) {
+                elem.setAttribute("id", "selected");
+            } else {
+                elem.setAttribute("id", "");
+            }
         }
-    })
+    );
 
     const filter_page = (page_str, num_notes) => {
         if (page_str === null) {
             return 1;
         } else {
-            const n = Number(page_str)
+            const n = Number(page_str);
             if (n < 1) {
                 return 1;
             }
@@ -90,4 +106,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     pageselect(page);
     display_pagenavigator(category, page, Math.ceil(num_notes / 8));
-})
+});
